79743d5f27d10dacb2bb4c7f1fc21a42eb30e8d4
Kirk Kaiser <kirk@zothcorp.com>
2025-01-09 17:55:53 -0500

    tinier but thats better

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ $ uv run manim  --resolution "1280,720" src/manim-communication.py Communication
 To generate gifs:
 
 ```
-$ uv run manim -r 800,600 --format=gif src/manim-communication.py CommunicationModel
+$ uv run manim -r 320,240 --format=gif src/manim-communication.py CommunicationModel
 ```
 
 Media files will be output into the `media/` directory.
--- generate_videos.sh
+++ generate_videos.sh
@@ -1,10 +1,10 @@
 
-uv run manim -r 800,600 --format=gif src/manim-communication.py CommunicationModel
-cp media/videos/manim-communication/600p60/CommunicationModel_ManimCE_v0.18.1.gif ./assets/communication.gif
+uv run manim -r 320,200 --format=gif src/manim-communication.py CommunicationModel
+cp media/videos/manim-communication/200p60/CommunicationModel_ManimCE_v0.18.1.gif ./assets/communication.gif
 uv run manim  --resolution "1280,720" src/manim-communication.py CommunicationModel
-uv run manim -r 800,600 --format=gif src/media-decision.py DecisionTreeAnimation
-cp media/videos/media-decision/600p60/DecisionTreeAnimation_ManimCE_v0.18.1.gif ./assets/media-decision.gif
+uv run manim -r 320,200 --format=gif src/media-decision.py DecisionTreeAnimation
+cp media/videos/media-decision/200p60/DecisionTreeAnimation_ManimCE_v0.18.1.gif ./assets/media-decision.gif
 uv run manim  --resolution "1280,720" src/media-decision.py DecisionTreeAnimation
-uv run manim -r 800,600 --format=gif src/radar-chart.py MultipleRadarCharts
-cp media/videos/radar-chart/600p60/MultipleRadarCharts_ManimCE_v0.18.1.gif ./assets/radar-chart.gif
+uv run manim -r 320,200 --format=gif src/radar-chart.py MultipleRadarCharts
+cp media/videos/radar-chart/200p60/MultipleRadarCharts_ManimCE_v0.18.1.gif ./assets/radar-chart.gif
 uv run manim  --resolution "1280,720" src/radar-chart.py MultipleRadarCharts
