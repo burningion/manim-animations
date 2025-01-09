6d9d2938128b628dae9448e1a2c15fc497021f33
Kirk Kaiser <kirk@zothcorp.com>
2025-01-09 09:06:21 -0500

    add another option. not quite there

--- src/media-decision.py
+++ src/media-decision.py
@@ -9,23 +9,22 @@ class DecisionTreeAnimation(Scene):
         decision_group = VGroup(decision_circle, decision_text)
         decision_group.shift(UP * 2)
 
-        # Create the bottom nodes
+        # Create the initial bottom nodes
         options = ["<b>TV Show</b>", "<b>Movie Theater</b>", "<b>Social Media</b>"]
         scores = ["Commitment: 30-60mins\n\nRewards: 6", "Commitment: 1-2.5 hrs\n\nRewards: 7.5", "Commitment: 15s-3.5hrs\n\nRewards: ??"]
         filenames = ["../video_assets/tv-show.svg", "../video_assets/movie-theater.svg", "../video_assets/social-media.svg"]
         bottom_circles = VGroup(*[SVGMobject(file).scale(.5) for file in filenames])
         bottom_texts = VGroup(*[MarkupText(text, font_size=24, color=BLACK, font="Helvetica") for text in options])
-        score_texts = VGroup(*[MarkupText(score, font_size=20, color=BLACK, font="Helvetica") for score in scores])
+        score_texts = VGroup(*[Text(score, font_size=20, color=BLACK, font="Helvetica") for score in scores])
         
         # Position bottom nodes
-        bottom_circles.arrange(RIGHT, buff=3)
+        bottom_circles.arrange(RIGHT, buff=3.0)
         bottom_circles.shift(DOWN * 2.5)
         
         for text, circle, score in zip(bottom_texts, bottom_circles, score_texts):
-            text.next_to(circle, DOWN, buff=.5)
-            score.next_to(text, DOWN, buff=.3)
+            text.next_to(circle, DOWN, buff=0.7)
+            score.next_to(text, DOWN, buff=0.5)
         
-        # Create complete bottom groups including scores
         bottom_groups = VGroup(*[
             VGroup(circle, text, score) 
             for circle, text, score in zip(bottom_circles, bottom_texts, score_texts)
@@ -36,7 +35,7 @@ class DecisionTreeAnimation(Scene):
             Arrow(
                 start=decision_circle.get_bottom() + DOWN * 1,
                 end=circle.get_top(),
-                buff=0.3,
+                buff=0.4,
                 stroke_width=4,
                 color=BLACK
             ) 
@@ -47,15 +46,15 @@ class DecisionTreeAnimation(Scene):
         for score in score_texts:
             score.set_opacity(0)
 
-        # First part of animation
+        # First part of animation (same as before)
         self.play(Create(decision_circle))
         self.play(Write(decision_text))
         for arrow in arrows:
             self.play(Create(arrow), run_time=1)
         for group in bottom_groups:
             self.play(
-                Create(group[0]),  # circle
-                Write(group[1]),   # text
+                Create(group[0]),
+                Write(group[1]),
                 run_time=1
             )
         
@@ -69,11 +68,10 @@ class DecisionTreeAnimation(Scene):
             run_time=1
         )
 
-        # Move bottom elements up and reveal scores
-        target_y = 0  # New vertical position (center of screen)
+        # Move bottom elements up
+        target_y = 0
         animations = []
         for group in bottom_groups:
-            # Create target position maintaining x-coordinate but changing y-coordinate
             target_pos = group.get_center().copy()
             target_pos[1] = target_y
             animations.append(group.animate.move_to(target_pos))
@@ -82,14 +80,44 @@ class DecisionTreeAnimation(Scene):
 
         # Fade in scores
         for group in bottom_groups:
-            score = group[2]  # The score text is the third element in each group
+            score = group[2]
             self.play(
                 score.animate.set_opacity(1),
                 run_time=0.5
             )
 
-        self.wait(2)
+        self.wait(5)
 
+        # Create new elements for Books and Podcast
+        new_options = ["<b>Book</b>", "<b>Podcast</b>"]
+        new_scores = ["Score: 9/10", "Score: 8/10"]
+        new_filenames = ["../video_assets/books.svg", "../video_assets/podcast.svg"]
+        
+        # Create new elements starting small and invisible
+        new_circles = VGroup(*[SVGMobject(file).scale(0.1) for file in new_filenames])  # Start very small
+        new_texts = VGroup(*[MarkupText(text, font_size=24, color=BLACK, font="Helvetica").set_opacity(0) 
+                            for text in new_options])
+        new_score_texts = VGroup(*[Text(score, font_size=20, color=BLACK, font="Helvetica") .set_opacity(0) 
+                                 for score in new_scores])
+
+        # Position new elements at the same x-coordinates as the elements they're replacing
+        for i, (new_circle, new_text, new_score) in enumerate(zip(new_circles, new_texts, new_score_texts)):
+            old_group = bottom_groups[i]
+            new_circle.move_to(old_group[0])
+            new_text.move_to(old_group[1])
+            new_score.move_to(old_group[2])
+
+        # Fade out old elements (first two groups) and fade in/expand new ones
+        for i in range(2):  # Only replace first two groups
+            self.play(
+                FadeOut(bottom_groups[i]),
+                new_texts[i].animate.set_opacity(1),
+                new_score_texts[i].animate.set_opacity(1),
+                new_circles[i].animate.scale(5),  # Scale up to final size
+                run_time=1
+            )
+
+        self.wait(2)
 if __name__ == "__main__":
     # Command to render:
     # manim -pqh decision_tree.py DecisionTreeAnimation
